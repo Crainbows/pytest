59092e4943f0b794e57957027364315cbce87ba4
Oliver Balmford <oli@oli.codes>
2019-03-07 16:06:42 +0000

    refactor: combine files and add symbolic link

--- .githooks/pre-commit/list-staged-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# shellcheck disable=SC2153
-if [ -n "${STAGED_FILES}" ]; then
-    echo "* Staged files:"
-
-    for STAGED_FILE in $STAGED_FILES; do
-        echo "  - $STAGED_FILE"
-    done
-fi
